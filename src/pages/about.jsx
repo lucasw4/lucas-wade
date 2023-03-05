9c6628184e1e38b5a05a84423cb7aed4e9e1714e
Lucas Sahota <lucaswade4@gmail.com>
2023-03-05 13:15:04 -0800

    Update styling, reword about section, smooth scroll behaviour

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,20 +9,17 @@ const About = () => {
       className='mt-72 mb-72 md:mt-72 md:mb-0 md:h-[44rem]'
     >
       <HeaderText message='About Me' />
-      <div className='lg:mx-32 flex flex-col h-[40rem] lg:justify-center lg:items-center'>
+      <div className='lg:mx-32 flex flex-col h-[40rem] lg:justify-center lg:items-center md:mt-24'>
         <div className='h-full flex flex-col justify-center lg:w-1/2'>
-          <div className='py-4 pl-4'>
-            I recently graduated from the Lighthouse Labs Full-Stack web
-            developer bootcamp and have been able to develop my skills in the
-            latest web development technologies. I have experience in both
-            front-end and back-end development and strive to create designs that
-            are not only visually appealing but also highly functional and
-            user-friendly.
-          </div>
-          <div className='py-2 pl-4'>
-            Whether you are seeking a dynamic and engaging website or a reliable
-            web application, I have the expertise and dedication to turn your
-            vision into a reality. Let me bring your online presence to life.
+          <div className='py-4 pl-4 text-2xl'>
+            I&apos;m a <strong className='text-purple-500'>Full-Stack </strong>
+            web developer, but I focus on mainly{" "}
+            <strong className='text-purple-500'>front-end </strong>
+            development. I am constantly learning to improve my skills and love
+            learning the latest technologies. Currently I am focused on learning
+            <strong className='text-purple-500'> NextJS</strong> and improving
+            my skills with <strong className='text-purple-500'>Tailwind</strong>
+            .
           </div>
         </div>
         <div className='mx-16 md:w-fit md:mx-auto md:p-4 border border-gray-700 flex justify-center py-3 mb-10 transition-all hover:shadow-lg hover:shadow-black hover:scale-110'>
